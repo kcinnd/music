4fc82164815a99d15852a226dd6314962ea16cee
kcinnd <152316054+kcinnd@users.noreply.github.com>
2024-02-20 16:26:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,28 @@ document.addEventListener("DOMContentLoaded", function() {
         ['rgba(75, 0, 130, 1)', 'rgba(75, 0, 130, 0)'],
         ['rgba(252, 142, 172, 1)', 'rgba(252, 142, 172, 0)'],
         ['rgba(0, 255, 195, 1)', 'rgba(0, 255, 195, 0)']
+    ];
+
+                          const noteImages = [
+        "https://i.imgur.com/CJ09TQq.png",
+        "https://i.imgur.com/b5fsfMv.png",
+        "https://i.imgur.com/iAKvZfs.png",
+        "https://i.imgur.com/qBMF0fz.png",
+        "https://i.imgur.com/OGFu1Le.png",
+        "https://i.imgur.com/iEUHvIc.png",
+        "https://i.imgur.com/14w5V4V.png",
+        "https://i.imgur.com/U0aP5pi.png",
+        "https://i.imgur.com/ZbZ7sqd.png",
+        "https://i.imgur.com/Y6GtKc3.png",
+        "https://i.imgur.com/vxsTh2E.png",
+        "https://i.imgur.com/nttMAYw.png",
+        "https://i.imgur.com/lkUVei7.png",
+        "https://i.imgur.com/MENQtiW.png",
+        "https://i.imgur.com/GXPMynI.png",
+        "https://i.imgur.com/Mlj0CoI.png",
+        "https://i.imgur.com/NmYfUe8.png",
+        "https://i.imgur.com/Mz5cEJR.png",
+        "https://i.imgur.com/IVHM0uZ.png"
     ].map(src => {
         const img = new Image();
         img.src = src;
